986abd1353a032fbc51851413feec2e2d807eca2
Alik Rakhmonov <alik.rahmonov@gmail.com>
2023-02-10 18:51:35 +0200

    feat: add amplify

--- apps/app/src/main.tsx
+++ apps/app/src/main.tsx
@@ -1,7 +1,10 @@
+import { Amplify } from 'aws-amplify';
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import App from './app/app';
+
+Amplify.configure({});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
